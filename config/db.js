2ba980f107832ebef1a980871b632d2fe575812d
samuelbwr <samuelbwr@gmail.com>
2015-11-09 23:29:22 +0000

    Refector db file

--- config/db.js
+++ config/db.js
@@ -1,21 +1,9 @@
 var mongoose = require('mongoose');
  
 var mongoIP = process.env.IP || 'localhost';
-
 var mongoPort = 27017;
-
 var mongoDb = "test";
  
 mongoose.connect('mongodb://'+mongoIP+':'+mongoPort+'/'+mongoDb);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function (callback) {
-    console.log("The connection was successfully created!");
-});
-
-// db.close();
 
 module.exports = mongoose;
